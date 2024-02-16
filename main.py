17d246c57c14321ada687f978cd2d957eee7e853
Pratham Agarwal <143127428+Pratham-815@users.noreply.github.com>
2024-02-16 20:21:35 +0530

    making the calculator recursive

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 def add(n1, n2):
     return n1+n2
 
@@ -21,21 +23,41 @@ operations = {
     "/" : divide 
 }
 
+def calculator():
+    is_on = True
 
-n1 = int(input("Enter the first number: "))
+    n1 = int(input("Enter the first number: "))
 
-for symbol in operations:
-    print(symbol)
+    for symbol in operations:
+        print(symbol)
 
-operation_symbol = input("Pick an operation from the above options: ")
+    operation_symbol = input("Pick an operation from the above options: ")
 
-n2 = int(input("Enter the second number: "))
+    n2 = int(input("Enter the second number: "))
 
-calculation_function = operations[operation_symbol]
-answer = calculation_function(n1, n2)
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(n1, n2)
 
-print(f"{n1} {operation_symbol} {n2} = {answer}")
+    print(f"{n1} {operation_symbol} {n2} = {answer}")
+
+    while is_on:
+        choice = input(f"Type 'y' to continue calculating with {answer}, type 'n' to exit, or type 'z' to start new calculation: ")
+
+        if choice=='n':
+            is_on = False
+            print(f"Exiting the calculator!!!...\n")
+        
+        elif choice=='y':
+            n3 = int(input("Enter the next number: "))
+            operation_symbol = input("Pick an operation: ")
+            calculation_function = operations[operation_symbol]
+            answer2 = calculation_function(answer, n3)
+            print(f"{answer} {operation_symbol} {n3} = {answer2}")
+
+        else:
+            is_on = False
+            os.system('cls')
+            calculator()
 
 
-
-
+calculator()
